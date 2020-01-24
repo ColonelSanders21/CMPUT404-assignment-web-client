d10b3debc4d29cd394a38b5a1cf5c1d0595b9754
Anders Johnson <anders.johnson.email@gmail.com>
2020-01-23 19:09:13 -0700

    GET works, but tests take VERY long to complete.

--- httpclient.py
+++ httpclient.py
@@ -33,7 +33,19 @@ class HTTPResponse(object):
         self.body = body
 
 class HTTPClient(object):
-    #def get_host_port(self,url):
+    def get_host_port_path(self,url):
+        # Returns the host and port from the url string.
+        parsed_url = urllib.parse.urlparse(url)
+        host = parsed_url.hostname
+        port = parsed_url.port
+        path = parsed_url.path
+        if port == None:
+            # We assume we are trying to access port 80 if there is no port defined.
+            port = 80
+        if path == '':
+            # We didn't request anything... so let's request /
+            path = '/'
+        return (host, port, path)
 
     def connect(self, host, port):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -68,8 +80,19 @@ class HTTPClient(object):
         return buffer.decode('utf-8')
 
     def GET(self, url, args=None):
-        code = 500
-        body = ""
+        host, port, path = self.get_host_port_path(url)
+        self.connect(host, port)
+        data = "GET %s HTTP/1.1\r\nHost: %s:%s\r\n\r\n" % (path, host, port)
+        self.sendall(data)
+        response = self.recvall(self.socket)
+        print('========')
+        print("We sent:", data, '\n')
+        print(response)
+        print('========\n')
+        # We strip out the code and body, and send as an HTTPResponse.
+        code = int(response.split(' ')[1])
+        body = response.split('\r\n\r\n')[1]
+        self.socket.close()
         return HTTPResponse(code, body)
 
     def POST(self, url, args=None):
@@ -90,6 +113,6 @@ if __name__ == "__main__":
         help()
         sys.exit(1)
     elif (len(sys.argv) == 3):
-        print(client.command( sys.argv[2], sys.argv[1] ))
+        print(client.command( sys.argv[2], sys.argv[1] ).body)
     else:
         print(client.command( sys.argv[1] ))
